d9986cf5110da5dd7689a460e8ee13d1558bf0bd
aniruddhabarapatre <aniruddhabarapatre@gmail.com>
2015-06-12 22:20:35 -0400

    Create events hash

--- mchat.js
+++ mchat.js
@@ -10,6 +10,20 @@ if (Meteor.isClient) {
       }, limit: 20
     });
   };
+
+  Template.input.events({
+    'click #send': function() {
+      var message = $('#newMessage').val();
+      var username = $('#username').val();
+      if (!message || !username) {
+        alert('Fill out both the fields.');
+      }
+      Meteor.saveMessage({
+        message: message,
+        username: username
+      });
+    }
+  });
 }
 
 if (Meteor.isServer) {
